551e26413916ccc42b3085e2fcb855fdf79ca0a9
Altiuss <altiuss@inbox.lv>
2022-07-05 22:33:46 +0200

    without commit

--- Homework03072022/src/FirstLevel2.java
+++ Homework03072022/src/FirstLevel2.java
@@ -1,20 +1,26 @@
 import java.util.Arrays;
 
-//Implement a method that returns the new array that is the given array in reverse order{10, 3, 3, 5, -9} -> {-9, 5, 3, 3, 10}
+//Implement a method that returns the new array that is the
+// given array in reverse order{10, 3, 3, 5, -9} -> {-9, 5, 3, 3, 10}
 public class FirstLevel2 {
     public static void main(String[] args) {
         int[] array = {10, 3, 3, 5, -9};
 
         System.out.println(Arrays.toString(ReturnRevOrd(array)));
 
+
     }
     private static int[] ReturnRevOrd(int[] array){
-        for (int i = array.length -1; i >= 0; i--){
-            System.out.print(array[i] +" ");
-//        int[] array2 = new int[array.length];
-//        array2 = array;
-//        return array2;
+        int tmp;
+        int n = array.length;
+        for (int i = 0; i < n/2; i++){
+            tmp = array[n-i-1];
+            array[n-i-1] = array[i];
+            array[i] = tmp;
+
         }
+
+
 
        return array;
     }
--- Homework03072022/src/SecondLevel.java
+++ Homework03072022/src/SecondLevel.java
@@ -1,4 +1,18 @@
 //Попробовать самостоятельно придумать и реализовать алгоритм сортировки одномерного массива.
 // Допускается изучить известные методы и реализовать один из них.
 public class SecondLevel {
+    public static void main(String[] args) {
+        int a = (int)(Math.random()*6);
+        int[] mas1 = new int[a];
+        int[] mas2 = new int[a];
+        for (int i = 0; i < a; i++){
+            mas1[i] = (int)(Math.random()*6);
+            mas2[i] = (int)(Math.random()*6);
+            System.out.print(mas1[i]);
+            System.out.println();
+            System.out.print(mas2[i]);
+        }
+
+//        System.out.println(a);
+    }
 }
